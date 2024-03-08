7281b41469260b559c52e322805c8f537d5d8625
Gavin <gmorgenegg1@gmail.com>
2024-03-07 21:57:16 -0700

    DOGGO

--- CodeCourse.ino
+++ CodeCourse.ino
@@ -2,6 +2,14 @@
 
 void setup(){
     //if you can see this, you have jon's code!
+    /*
+          / \__
+     (    @\___
+     /         O
+    /   (_____/
+   /_____/   U
+  /     / \
+    */
 }
 
 void loop(){
