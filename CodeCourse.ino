0fe6b4d9e1f63e15cd172e7dc274fe37ec9d84b1
Hayden <haydenstout99@gmail.com>
2024-03-07 22:05:11 -0700

    "hi"

--- CodeCourse.ino
+++ CodeCourse.ino
@@ -2,9 +2,51 @@
 
 void setup(){
     //if you can see this, you have jon's code!
-    // Nuh Uh
+    
 }
 
 void loop(){
 
+    Serial.println("                        . - ~ ~ ~ - . \n");
+    Serial.println("      ..     _      .-~               ~-. \n");
+    Serial.println("     //|     \\ `..~                      `. \n");
+    Serial.println("    || |      }  }              /       \\  \\ \n");
+    Serial.println("(\\   \\ \\~^..'                 |         }  \\ \n");
+    Serial.println(" \\`.-~  o      /       }       |        /    \\ \n");
+    Serial.println(" (__          |       /        |       /      ` \n");
+    Serial.println("  `- - ~ ~ -._|      /_ - ~ ~ ^|      /- _      `. \n");
+    Serial.println("              |     /          |     /     ~-.     ~- \n");
+    Serial.println("              |_____|          |_____|         ~ - . _ _~_-_ \n");
+ 
+    Serial.println("        , \n");
+    Serial.println("       /| \n");
+    Serial.println("      / | \n");
+    Serial.println("     /  / \n");
+    Serial.println("    |   | \n");
+    Serial.println("   /    | \n");
+    Serial.println("   |    \\_ \n");
+    Serial.println("   |      \\__ \n");
+    Serial.println("   \\       __\\_______ \n");
+    Serial.println("    \\                 \\_ \n");
+    Serial.println("    | /                 \\ \n");
+    Serial.println("    \\/                   \\ \n");
+    Serial.println("     |                    | \n");
+    Serial.println("     \\                   \\| \n");
+    Serial.println("     |                    \\ \n");
+    Serial.println("     \\                     | \n");
+    Serial.println("     /\\    \\_               \\ \n");
+    Serial.println("    / |      \\__ (   )       \\ \n");
+    Serial.println("   /  \\      / |\\  /       __\\____ \n");
+    Serial.println("   |  ,     |  /\\ \\ \\__    |       \\_ \n");
+    Serial.println("   \\_/|\\___/   \\   \\}}}\\__|  (@)     ) \n");
+    Serial.println("    \\)\\)\\)      \\_\\---\\   \\|       \\ \\ \n");
+    Serial.println("                  \\>\\>\\>   \\   /\\__o_o) \n");
+    Serial.println("                            | /  VVVVV \n");
+    Serial.println("                            \\ \\    \\ \n");
+    Serial.println("                             \\ \\MMMMM                  oh shit! \n");
+    Serial.println("                              \\______/         _____ / \n");
+    Serial.println("                                              |  O O| \n");
+    Serial.println("                                             /___|_|/\\_ \n");
+    Serial.println("                                        ==( |          | \n");
+    Serial.println("                                             (o)====(o) \n");
 }
